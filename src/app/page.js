96d7a732ec9d0d80768d6eb449dffa352ad8a54b
Risky <notriskysh@gmail.com>
2024-09-22 11:37:36 +0530

    Minor fixes for small devices

--- src/app/page.js
+++ src/app/page.js
@@ -9,12 +9,12 @@ export default function Home() {
       <Header />
       </div>
       <div className="w-full h-96 grid mx-auto">
-        <div className="p-20 mx-auto w-8/12">
+        <div className="p-4 mx-auto w-8/12 mt-16">
           <h1 className="text-white text-5xl font-semibold flex relative h-2">Risky</h1>
           <p className="text-white font-medium flex mt-16">
             Hi i am so called Risky, a creative web developer</p>
           <p className="text-white font-medium mt-4">Specialized in <a className="underline underline-offset-4 decoration-[#808080] hover:decoration-[#fff] duration-700" href="https://react.dev" target="_blank">React</a>, <a className="underline underline-offset-4 decoration-[#808080] hover:decoration-[#fff] duration-700" href="https://nextjs.org" target="_blank">NextJS </a>
-           and <a className="underline underline-offset-4 decoration-[#808080] hover:decoration-[#fff] duration-700" href="/" target="_blank">SvelteKit</a> with strong knowledge in Javascript
+           and <a className="underline underline-offset-4 decoration-[#808080] hover:decoration-[#fff] duration-700" href="https://kit.svelte.dev" target="_blank">SvelteKit</a> with strong knowledge in Javascript
           </p>
           <p className="text-white font-medium">
             I have also decent experience with <a className="underline underline-offset-4 decoration-[#808080] hover:decoration-[#fff] duration-700" href="https://python.org" target="_blank">Python</a> and strong knowledge in building UIs<br></br>
