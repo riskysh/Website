05c5f913b5a4e4ccf336b25992653e4355688ab5
Risky <notriskysh@gmail.com>
2024-10-29 20:32:53 +0530

    minor fix

--- src/components/project-box.jsx
+++ src/components/project-box.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export default function ProjectBox({title, description, link}) {
     return (
-      <div className="w-fit grid gap-2 cursor-pointer text-white border border-[#1e1e1e] rounded-lg p-4 hover:bg-[#1e1e1e] duration-300">
+      <div className="w-fit grid gap-2 text-white border border-[#1e1e1e] rounded-lg p-4 hover:bg-[#1e1e1e] duration-300">
           <Link href={link} target="_blank">
-              <h1 className="text-2xl font-semibold">{title}</h1>
+              <h1 className="text-2xl font-semibold underline underline-offset-4 decoration-1">{title}</h1>
           </Link>
           <p className="font-medium">{description}</p>
       </div>
